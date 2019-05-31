c539840d42e45d83d106ad02b365c82f93f1c2d4
Gaurav Saxena <gauravsaxena.ec@gmail.com>
2019-05-31 07:28:44 +0200

    commit without test

---
+++ src/main/java/Algebra.java
@@ -0,0 +1,10 @@
+public class Algebra {
+
+    public int addNumbers(int a, int b){
+        return a+b;
+    }
+
+    public double divideNumbers(int a, int b) throws ArithmeticException{
+        return a/b;
+    }
+}
